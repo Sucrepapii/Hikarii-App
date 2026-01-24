85624ee6849635d57066faae4c92be9ee62ed8a7
Samuel <akinboroo@gmail.com>
2026-01-24 18:07:01 +0100

    Fix: Correct start script path for compiled output

--- dist-server/app.js
+++ dist-server/app.js
@@ -1,12 +1,12 @@
 import app from "./server";
 import express from "express";
 import path from "path";
-import { fileURLToPath } from "url";
+// import { fileURLToPath } from "url";
 // Helper for ESM directory
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 // Serve static files from the React app
-const clientBuildPath = path.join(__dirname, "../../../dist");
+const clientBuildPath = path.join(process.cwd(), "dist");
 app.use(express.static(clientBuildPath));
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
